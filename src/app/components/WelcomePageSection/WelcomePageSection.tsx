dc09c43acffd7caa5de7862e261d2af4fc3a3896
Matheus <matheus.mleall@gmail.com>
2024-04-19 23:35:38 -0300

    created animation and scroll function

--- src/app/components/WelcomePageSection/WelcomePageSection.tsx
+++ src/app/components/WelcomePageSection/WelcomePageSection.tsx
@@ -2,10 +2,24 @@
 import ArrowIcon from "@/icons/ArrowIcon";
 import TitleGradientComponent from "@/app/components/TitleGradientComponent/TitleGradientComponent";
 import styles from "./WelcomePageSection.module.css";
+import { useRef } from "react";
 
 export default function WelcomePageSection() {
+	const sectionRef = useRef<HTMLTableSectionElement>(null);
+	const handleSendToAbout = () => {
+		const screenSize = sectionRef.current?.clientHeight;
+		console.log(screenSize);
+		// 0, screenSize + 70
+		if (screenSize) {
+			window.scroll({
+				top: screenSize + 70,
+				left: 100,
+				behavior: "smooth",
+			});
+		}
+	};
 	return (
-		<section className={styles.welcomePageContainer}>
+		<section ref={sectionRef} className={styles.welcomePageContainer}>
 			<div className={styles.infoContainer}>
 				<div className={styles.pictureWrap}></div>
 				<div className={styles.textContainer}>
@@ -16,7 +30,12 @@ export default function WelcomePageSection() {
 						titleText='Think, Develop and Share.'
 					/>
 					<p className={styles.subTitleText}>Fullstack Software Developer.</p>
-					<ArrowIcon />
+					<div
+						onClick={handleSendToAbout}
+						className={styles.arrowIconContainer}
+					>
+						<ArrowIcon />
+					</div>
 				</div>
 			</div>
 		</section>
