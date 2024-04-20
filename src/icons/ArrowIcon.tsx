69cc6926df237f22a2f65d680e22ec1f15a3c287
Matheus <matheus.mleall@gmail.com>
2024-04-19 23:05:26 -0300

    added arrow icon

---
+++ src/icons/ArrowIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ArrowIcon = () => {
+	return (
+		<svg
+			width='28'
+			height='14'
+			viewBox='0 0 28 14'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M2 2L9.86126 10.2835C12.273 12.8248 16.3399 12.7678 18.6795 10.1599L26 2'
+				stroke='#5A5757'
+				strokeWidth='3'
+				strokeLinecap='round'
+			/>
+		</svg>
+	);
+};
+
+export default ArrowIcon;
