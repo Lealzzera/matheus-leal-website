fa1c7e4a434127b1f4eb289c90c6ea712c88c6c6
Matheus Leal <matheus.mleall@gmail.com>
2025-09-01 22:48:23 -0300

    feat: replaced the subtitle text

--- src/app/components/WelcomePageSection/WelcomePageSection.tsx
+++ src/app/components/WelcomePageSection/WelcomePageSection.tsx
@@ -5,37 +5,37 @@ import styles from "./WelcomePageSection.module.css";
 import { useRef } from "react";
 
 export default function WelcomePageSection() {
-	const sectionRef = useRef<HTMLTableSectionElement>(null);
-	const handleSendToAbout = () => {
-		const screenSize = sectionRef.current?.clientHeight;
+  const sectionRef = useRef<HTMLTableSectionElement>(null);
+  const handleSendToAbout = () => {
+    const screenSize = sectionRef.current?.clientHeight;
 
-		if (screenSize) {
-			window.scroll({
-				top: screenSize + 40,
-				left: 100,
-				behavior: "smooth",
-			});
-		}
-	};
-	return (
-		<section ref={sectionRef} className={styles.welcomePageContainer}>
-			<div className={styles.infoContainer}>
-				<div className={styles.textContainer}>
-					<TitleGradientComponent
-						fontDesktopSize='2rem'
-						fontMobileSize='1.25rem'
-						textColor='-webkit-linear-gradient(left, var(--light-green), var(--light-blue))'
-						titleText='Think, Develop and Share.'
-					/>
-					<p className={styles.subTitleText}>A Fullstack Software Developer.</p>
-					<div
-						onClick={handleSendToAbout}
-						className={styles.arrowIconContainer}
-					>
-						<ArrowIcon />
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    if (screenSize) {
+      window.scroll({
+        top: screenSize + 40,
+        left: 100,
+        behavior: "smooth",
+      });
+    }
+  };
+  return (
+    <section ref={sectionRef} className={styles.welcomePageContainer}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <TitleGradientComponent
+            fontDesktopSize="2rem"
+            fontMobileSize="1.25rem"
+            textColor="-webkit-linear-gradient(left, var(--light-green), var(--light-blue))"
+            titleText="Think, Develop and Share."
+          />
+          <p className={styles.subTitleText}>A Software Engineer.</p>
+          <div
+            onClick={handleSendToAbout}
+            className={styles.arrowIconContainer}
+          >
+            <ArrowIcon />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
